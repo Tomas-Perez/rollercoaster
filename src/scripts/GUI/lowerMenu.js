d37c38c2e667a059edfbc3e9266a103f9815458f
Tomas Perez Molina <tomas.perezmolina@ing.austral.edu.ar>
2017-08-24 20:50:38 -0300

    minor bar fix

--- src/scripts/GUI/lowerMenu.js
+++ src/scripts/GUI/lowerMenu.js
@@ -17,17 +17,17 @@ LowerMenu.prototype.display = function(potentialEnergy, kineticEnergy, elasticEn
     this.bars(potentialEnergy, kineticEnergy, elasticEnergy, mechanicEnergy, initialEnergy);
 };
 
-LowerMenu.prototype.bars = function (potentialEnergy, kineticEnergy, elasticEnergy, mechanicEnergy, initialEnergy) {
-    let potentialEnergy = round(potentialEnergy) || 0;
-    let kineticEnergy = round(kineticEnergy) || 0;
-    let elasticEnergy = round(elasticEnergy) || 0;
-    let mechanicEnergy = round(mechanicEnergy) || 0;
-    let initialEnergy = round(initialEnergy) || 0;
+LowerMenu.prototype.bars = function (potentialEnergy, kineticEnergy, elasticEnergy, mechanicalEnergy, initialEnergy) {
+    let actualPotential = round(potentialEnergy) || 0;
+    let actualKinetic = round(kineticEnergy) || 0;
+    let actualElastic = round(elasticEnergy) || 0;
+    let actualMechanical = round(mechanicalEnergy) || 0;
+    let actualInitial = round(initialEnergy) || 0;
 
-    let bar1Length = map(potentialEnergy, 0, initialEnergy, 0, 500);
-    let bar2Length = map(kineticEnergy, 0, initialEnergy, 0, 500);
-    let bar3Length = map(elasticEnergy, 0, initialEnergy, 0, 500);
-    let bar4Length = map(mechanicEnergy, 0, initialEnergy, 0, 500);
+    let bar1Length = map(actualPotential, 0, actualInitial, 0, 500);
+    let bar2Length = map(actualKinetic, 0, actualInitial, 0, 500);
+    let bar3Length = map(actualElastic, 0, actualInitial, 0, 500);
+    let bar4Length = map(actualMechanical, 0, actualInitial, 0, 500);
 
     let barSeparation = 30;
     let stringSeparation = 15;
@@ -35,33 +35,37 @@ LowerMenu.prototype.bars = function (potentialEnergy, kineticEnergy, elasticEner
     let barHeight = 20;
     let middleLowerMenuX = this.x + this.width/2;
     let middleLowerMenuY = this.y + this.height/2;
+    push();
     //bar 1
-    stroke(255);
+    noStroke();
     fill(255,0,0);
     rect(middleLowerMenuX, middleLowerMenuY - barSeparation, bar1Length, barHeight);
-    this.barText("Energia potencial: " + potentialEnergy + " N", stringSize,
+    this.barText("Energia potencial: " + actualPotential + " N", stringSize,
         middleLowerMenuX, middleLowerMenuY - barSeparation + stringSeparation);
     //bar 2
     fill(0,255,0);
     rect(middleLowerMenuX, middleLowerMenuY, bar2Length, barHeight);
-    this.barText("Energia cinetica: " + kineticEnergy + " N", stringSize,
+    this.barText("Energia cinetica: " + actualKinetic + " N", stringSize,
         middleLowerMenuX, middleLowerMenuY + stringSeparation);
     //bar 3
     fill(0,0,255);
     rect(middleLowerMenuX, middleLowerMenuY + barSeparation, bar3Length, barHeight);
-    this.barText("Energia elastica: " + elasticEnergy + " N", stringSize,
+    this.barText("Energia elastica: " + actualElastic + " N", stringSize,
         middleLowerMenuX, middleLowerMenuY + barSeparation+ stringSeparation);
     //bar 4
     fill(200,200,200);
     rect(middleLowerMenuX, middleLowerMenuY + 2*barSeparation, bar4Length, barHeight);
-    this.barText("Energia mecanica: " + mechanicEnergy + " N", stringSize,
+    this.barText("Energia mecanica: " + actualMechanical + " N", stringSize,
         middleLowerMenuX, middleLowerMenuY + 2*barSeparation+ stringSeparation);
+    pop();
 };
 
 LowerMenu.prototype.barText = function(string, stringSize, x, y){
+    push();
     fill(0);
     textSize(stringSize);
     //textStyle(BOLD);
     stroke(0);
     text(string,x, y);
+    pop();
 };
