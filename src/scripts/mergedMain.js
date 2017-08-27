b70fe6c3ca287214615011768dd4395003456bc8
arimirra <arimirra@hotmail.com>
2017-08-27 16:57:04 -0300

    branched

--- src/scripts/mergedMain.js
+++ src/scripts/mergedMain.js
@@ -1,32 +1,45 @@
 let canvas;
 let lowerMenu;
 let varRightMenu;
-let expRightMenu;
 
 function setup() {
     let width = 1024;
     let height = 768;
-    let parent = "canvasDiv";
+    let chartsWidth = 250;
+    let chartsHeight = 250;
+    let containerDivId = "container";
+    let canvasDivId = "canvasDiv";
+    let chartsDivId = "chartsDiv";
 
-    let canvasDiv = document.getElementById(parent);
+    exercise = new Exercise();
+
+    //container formatting
+    let container = document.getElementById(containerDivId);
+    container.style.width = width + chartsWidth + "px";
+    container.style.height = height + "px";
+    //charts div formatting
+    let chartsDiv = document.getElementById(chartsDivId);
+    chartsDiv.parent = container;
+    chartsDiv.style.width = chartsWidth + "px";
+    chartsDiv.style.height = height + "px";
+    //canvas div formatting (the simulation)
+    let canvasDiv = document.getElementById(canvasDivId);
+    canvasDiv.parent = container;
     canvasDiv.style.width = width + "px";
     canvasDiv.style.height = height + "px";
 
+    //simulation canvas creation
     canvas = createCanvas(width, height);
-    exercise = new Exercise();
     canvas.addClass('canvas');
-    canvas.parent(parent);
-    lowerMenu = new LowerMenu(-1, canvas.height*3/4, canvas.width+1, canvas.height*1/4, [179, 229, 252], parent);
+    canvas.parent(canvasDiv);
+    background(0);
 
-    expRightMenu = new RightMenu(canvas.width*1/4, canvas.height, "expRightMenu", "rightMenu", parent, "Experiences");
-    expRightMenu.addContent(new ExpThumbnail(expRightMenu.width, 70, expRightMenu.height, "../assets/changeExercise.png", expRightMenu.id, 0, function(){ return toggleMenu('varbtn', 'expRightMenu');}));
-    expRightMenu.addContent(new ExpThumbnail(expRightMenu.width, 70, expRightMenu.height, "../assets/changeExercise.png", expRightMenu.id, 1, function(){ return toggleMenu('varbtn', 'expRightMenu');}));
-    expRightMenu.addContent(new ExpThumbnail(expRightMenu.width, 70, expRightMenu.height, "../assets/changeExercise.png", expRightMenu.id, 2, function(){ return toggleMenu('varbtn', 'expRightMenu');}));
+    lowerMenu = new LowerMenu(-1, canvas.height*3/4, canvas.width+1, canvas.height*1/4, [179, 229, 252], canvasDiv);
+    varRightMenu = new RightMenu(canvas.width*1/4, canvas.height,"varRightMenu", "rightMenu", canvasDiv, "Variables");
+    varRightMenu.addVariable(new Variable("gravity", "m", varRightMenu.width, varRightMenu.height, varRightMenu.id, 0));
+    varRightMenu.addVariable(new Variable("gravity", "m", varRightMenu.width, varRightMenu.height, varRightMenu.id, 1));
 
-
-    varRightMenu = new RightMenu(canvas.width*1/4, canvas.height, "varRightMenu", "rightMenu", parent, "Variables");
-    varRightMenu.addContent(new Variable("gravity", "m", varRightMenu.width, varRightMenu.height, varRightMenu.id, 0));
-    varRightMenu.addContent(new Variable("gravity", "m", varRightMenu.width, varRightMenu.height, varRightMenu.id, 1));
+    createChart(chartsDiv, "chartsDiv", "barChart", chartsWidth, chartsHeight);
 }
 
 function draw() {
