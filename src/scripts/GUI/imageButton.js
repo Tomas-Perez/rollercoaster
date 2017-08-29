31f0788b0738f6fb96af9c943d69d4508d41a5b9
Manuel Pedrozo <manuel.pedrozo@ing.austral.edu.ar>
2017-08-29 11:16:45 -0300

    working with button actions

--- src/scripts/GUI/imageButton.js
+++ src/scripts/GUI/imageButton.js
@@ -1,4 +1,9 @@
-function ImageButton(x, y, width, height, path, parent, classs, id, onClick) {
+function ImageButton(x, y, width, height, path, parent, classs, id, onClick, hoverPath, pressedPath) {
+    this.path = path;
+    this.pressedPath = pressedPath;
+    this.hoverPath = hoverPath;
+    this.id = id;
+    this.onClick = onClick;
     this.btn = createInput(0,"image");
     this.btn.attribute('id', id);
     this.btn.attribute('src', path);
@@ -8,6 +13,19 @@ function ImageButton(x, y, width, height, path, parent, classs, id, onClick) {
     this.btn.style('height', height + 'px');
     this.btn.parent(parent);
     this.btn.addClass(classs);
-    this.btn.mousePressed(onClick);
-    //this.btn.mousedown(onClick);
+    this.btn.mousePressed(this.clicked.bind(this));
+    this.btn.mouseReleased(this.changeImage.bind(this,this.hoverPath));
+    this.btn.mouseOver(this.changeImage.bind(this,this.hoverPath));
+    this.btn.mouseOut(this.changeImage.bind(this,this.path));
 }
+
+ImageButton.prototype.changeImage = function (path) {
+    this.btn.attribute('src', path);
+};
+
+ImageButton.prototype.clicked = function(){
+    this.onClick();
+    this.changeImage(this.pressedPath)
+};
+
+
